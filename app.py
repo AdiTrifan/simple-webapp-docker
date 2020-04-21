bb552f666c9514f3bda03965053e8e5300ecf65b
AdiTrifan <adi.trif_an@yahoo.com>
2020-04-21 15:46:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Welcome! The file has been updated by Adrian Trifan "
 
 @app.route('/how are you')
 def hello():
